6a49ee73b8f46bf9b30601a4aa85cdb8153f5c7e
jasmithareddy23 <jasmithareddy23@gmail.com>
2026-05-13 12:40:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const cards = document.querySelectorAll('.project-card, .about-card, .skill-card');
+
+window.addEventListener('scroll', () => {
+
+  cards.forEach(card => {
+
+    const cardTop = card.getBoundingClientRect().top;
+
+    if(cardTop < window.innerHeight - 100) {
+      card.style.opacity = '1';
+      card.style.transform = 'translateY(0)';
+    }
+  });
+
+});
+
+cards.forEach(card => {
+  card.style.opacity = '0';
+  card.style.transform = 'translateY(40px)';
+  card.style.transition = 'all 0.8s ease';
+});
